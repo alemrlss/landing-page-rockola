b972828ef55869d17d5d3ee905bc837aaf44ea6e
unknown <Alejandro>
2024-02-28 20:46:57 -0400

    landing

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -3,7 +3,7 @@ import PresentationText from "./PresentationText";
 
 function Presentation() {
   return (
-    <div className="flex flex-col items-center pt-24 sm:flex-row mx-0 sm:mx-20 w-full g">
+    <div className="flex flex-col items-center pt-24 sm:flex-row mx-0 sm:mx-20 w-full z-10">
       <PresentationText />
       <PresentationImage />
     </div>
